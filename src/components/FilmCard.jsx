3c2a121cb7183834781c044f6b2bfb182e107b2f
bric12369 <barryrichards05@gmail.com>
2025-08-28 16:52:40 +0100

    formatted dates and handled date not found

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -1,12 +1,15 @@
+import formatDate from "../../utils/format-date"
+
 
 const placeholderPoster = '../public/tumbleweed-silence.gif'
 
 const FilmCard = ({ film }) => {
     const poster = film.poster_path === null ? placeholderPoster : `https://image.tmdb.org/t/p/w500${film.poster_path}`
+    const releaseDate = formatDate(film.release_date)
     return (
         <div key={film.id} id='filmCard'>
             <h3>{film.title}</h3>
-            <p>{film.release_date}</p>
+            {releaseDate !== '' ? <p>Released: {releaseDate}</p> : <p>Release date could not be found</p>}
             <div id='posterContainer'>
                 <img src={poster} />
                 {poster === placeholderPoster && <figcaption>No poster found...</figcaption>}
