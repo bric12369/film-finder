45b93c6a03d307089f16bd9bfd198fa2dc079421
bric12369 <barryrichards05@gmail.com>
2025-08-28 13:32:06 +0100

    added loading state and conditional render

--- src/components/FilmList.jsx
+++ src/components/FilmList.jsx
@@ -2,32 +2,43 @@ import { useState, useEffect } from "react"
 
 const FilmList = ({ searchTerm, apiKey }) => {
     const [films, setFilms] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
+        setIsLoading(true)
         fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchTerm}`)
-            .then(res => res.json())
+            .then(res => {
+                return res.json()
+            })
             .then(data => {
                 console.log(data.results)
-                setFilms(data.results)});
+                setFilms(data.results)
+                setIsLoading(false)
+            });
     }, [searchTerm])
-    return (
-        <>
-        {searchTerm !== '' && films.length > 0 && <h2>Search Results: {searchTerm}</h2>}
-        {searchTerm !== '' && films.length === 0 && <h2>Sorry... No results for {searchTerm}</h2>}
-        <ul>
-            {films.map((film) => {
-                return(
-                    <li key={film.id}>
-                        <h3>{film.title}</h3>
-                        <p>{film.release_date}</p>
-                        <img src={`https://image.tmdb.org/t/p/w500${film.poster_path}`} />
-                        <p>{film.overview}</p>
-                    </li>
-                )
-            })}
-        </ul>
+    return isLoading ? (<>
+        <div className="spinner-container">
+            <div className="spinner"></div>
+            <p>Loading...</p>
+        </div>
+    </>) :
+        (<>
+            {searchTerm !== '' && films.length > 0 && <h2>Search Results: {searchTerm}</h2>}
+            {searchTerm !== '' && films.length === 0 && <h2>Sorry... No results for {searchTerm}</h2>}
+            <ul>
+                {films.map((film) => {
+                    return (
+                        <li key={film.id}>
+                            <h3>{film.title}</h3>
+                            <p>{film.release_date}</p>
+                            <img src={`https://image.tmdb.org/t/p/w500${film.poster_path}`} />
+                            <p>{film.overview}</p>
+                        </li>
+                    )
+                })}
+            </ul>
         </>
-    )
+        )
 }
 
 export default FilmList
