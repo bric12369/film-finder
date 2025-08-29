b173f1e6a6d260a34c7f43936df94499aaea4249
bric12369 <barryrichards05@gmail.com>
2025-08-29 12:20:26 +0100

    corrected filepath of placeholderPoster

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -1,7 +1,7 @@
 import formatDate from "../../utils/format-date"
 
 
-const placeholderPoster = '../public/tumbleweed-silence.gif'
+const placeholderPoster = '/tumbleweed-silence.gif'
 
 const FilmCard = ({ film }) => {
     const poster = film.poster_path === null ? placeholderPoster : `https://image.tmdb.org/t/p/w500${film.poster_path}`
